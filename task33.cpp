99d1aced451aa20bc67289f68995e2e0797dcf9d
Barnich-Kate <85077866+Barnich-Kate@users.noreply.github.com>
2023-05-29 20:59:40 +0300

    Create task33.cpp
    Add the solution of the task number 33

---
+++ task33.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+		
+	double x, a, y, price, cost;
+	cout << "Type the price of sweets";
+	cin >> a;
+	cout << "Type the weigth of sweets";
+	cin >> x;
+	price = a / x;
+	cost = price * y;
+	cout << "Price:" << price << endl;
+	cout << "Cost:" << cost << endl;
+	return 0;
+
+
+}
+    
