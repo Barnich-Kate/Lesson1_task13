29bf8be68b66ebc9c37ac1ddd8345083ea3d5774
Barnich-Kate <85077866+Barnich-Kate@users.noreply.github.com>
2023-05-29 20:48:55 +0300

    Create task_23.cpp
    Solution of the task number 23

---
+++ task_23.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+		
+	double A, B, C;
+	A = 5;
+	B = 6;
+	C = 7;
+	B = A;
+	C = B;
+	A = C;
+	cout << A << "  ";
+	cout << B << "  ";
+	cout << C;
+	return 0;
+
+
+}
