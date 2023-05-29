eb54459e190d43eaa1561455590ee882c1dc4c0b
Barnich-Kate <85077866+Barnich-Kate@users.noreply.github.com>
2023-05-29 20:50:27 +0300

    Create task25.cpp
    Solution of the task 25

---
+++ task25.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+		
+	double y, x;
+	cout << "Type x";
+	cin >> x;
+	y = 3*(x*x*x*x*x*x*x) - 6*(x*x) - 7;
+	cout << "y:" << y ;
+	return 0;
+
+
+}
